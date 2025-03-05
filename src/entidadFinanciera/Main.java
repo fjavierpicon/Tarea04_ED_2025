281812d6402e858cdf47dbc0f8cf89d3eea956b8
fpiccab671 <fpiccab671@SOBREMESA>
2025-03-05 21:13:28 +0100

    Introducción de los métodos probarRetirar y ProbarIngresar Introducción de un nuevo parámetro cantidadEsperada

--- src/entidadFinanciera/Main.java
+++ src/entidadFinanciera/Main.java
@@ -18,5 +18,25 @@ public class Main {
         saldoActual = objetoCuenta.estado();
         System.out.println("El saldo actual es"+ saldoActual );
 
+        probarRetirada(objetoCuenta, objetoCuenta);
+        
+        probarIngresar(objetoCuenta, objetoCuenta);
+        
+    }
+
+    public static void probarIngresar(CCuenta objetoCuenta, CCuenta cantidadEsperada) {
+        try {
+            cantidadEsperada.ingresar(1200);
+        } catch (Exception e) {
+            System.out.println("No se pudo realizar el ingreso en cuenta");
+        }
+    }
+
+    public static void probarRetirada(CCuenta objetoCuenta, CCuenta cantidadEsperada) {
+        try {
+            cantidadEsperada.retirar(2400);
+        } catch (Exception e) {
+            System.out.println("Error al retirar la cantidad indicada");
+        }
     }
 }
